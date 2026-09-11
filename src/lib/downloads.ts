3647d408302bff0a0a5b216d797f593b33cb270d
Luis Angulo Ruiz <143514041+LuisRz1@users.noreply.github.com>
2026-09-11 12:05:17 -0500

    chore(landing): APK movil 0.5.0

--- src/lib/downloads.ts
+++ src/lib/downloads.ts
@@ -1,8 +1,8 @@
 export const mobileRelease = {
-	version: "0.4.1",
+	version: "0.5.0",
 	androidUrl:
-		"https://github.com/LuisRz1/battlegraf/releases/download/v0.4.1/battlegraf-android.apk",
+		"https://github.com/LuisRz1/battlegraf/releases/download/v0.5.0/battlegraf-android.apk",
 	androidFileName: "battlegraf-android.apk",
-	androidSize: "59 MB",
-		androidSha256: "2FAA50479666B40C71A98E8DCCF6B7CB0C50D85E5F37E6735CA179811DBF20C4",
+	androidSize: "56 MB",
+		androidSha256: "5E2D99EF27432A93240109DED531BC5E91FD31A5B85271A59C3AFFFAF951E10E",
 } as const;
