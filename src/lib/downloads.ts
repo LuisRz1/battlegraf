fe70adee3cca38e5bbab67b7d44ec14f17be36b8
Luis Angulo Ruiz <143514041+LuisRz1@users.noreply.github.com>
2026-09-11 14:42:59 -0500

    chore(landing): APK movil 0.7.0 (mapas dinamicos y nueva paleta)

--- src/lib/downloads.ts
+++ src/lib/downloads.ts
@@ -1,8 +1,8 @@
 export const mobileRelease = {
-	version: "0.6.1",
+	version: "0.7.0",
 	androidUrl:
-		"https://github.com/LuisRz1/battlegraf/releases/download/v0.6.1/battlegraf-android.apk",
+		"https://github.com/LuisRz1/battlegraf/releases/download/v0.7.0/battlegraf-android.apk",
 	androidFileName: "battlegraf-android.apk",
 	androidSize: "56 MB",
-		androidSha256: "EDCF8DF41F660E1D496D8EFF5BF6D1C7F23E38CEF6C5AFFA6D560DCA65EC99E8",
+		androidSha256: "DBFF444574F264A6020366F9BB75D3D071ACB95EA66C1C0DCAA8885E8BA42FF5",
 } as const;
